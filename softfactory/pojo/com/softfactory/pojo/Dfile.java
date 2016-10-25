5869ffa214f19ffb97067ab0152c9818bd373550
NovakRonaldo <872175906@qq.com>
2016-10-25 14:36:37 +0800

    凤凤

--- softfactory/pojo/com/softfactory/pojo/Dfile.java
+++ softfactory/pojo/com/softfactory/pojo/Dfile.java
@@ -27,9 +27,9 @@ public class Dfile implements Serializable {
 	private String productClass;// 档次级别 D001-1:高档 D001-2:中档 D001-3:低档
 	private String personalUnit;// 计量单位
 	private String personalValue;// 计量值
-	private double listPrice;// 市场单价，not null
-	private double costPrice;// 计划成本单价，not null
-	private double realCostPrice;// 成本单价
+	private Double listPrice;// 市场单价，not null
+	private Double costPrice;// 计划成本单价，not null
+	private Double realCostPrice;// 成本单价
 	private String amountUnit;// 单位
 	private String productDescribe;// 产品描述
 	private String responsiblePerson;// 产品经理
@@ -57,280 +57,281 @@ public class Dfile implements Serializable {
 		return id;
 	}
 
-	public String getProductId() {
-		return productId;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public String getFactoryName() {
-		return factoryName;
-	}
-
-	public String getFirstKindId() {
-		return firstKindId;
-	}
-
-	public String getFirstKindName() {
-		return firstKindName;
-	}
-
-	public String getSecondKindId() {
-		return secondKindId;
-	}
-
-	public String getSecondKindName() {
-		return secondKindName;
-	}
-
-	public String getThirdKindId() {
-		return thirdKindId;
-	}
-
-	public String getThirdKindName() {
-		return thirdKindName;
-	}
-
-	public String getProductNick() {
-		return productNick;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public String getProductClass() {
-		return productClass;
-	}
-
-	public String getPersonalUnit() {
-		return personalUnit;
-	}
-
-	public String getPersonalValue() {
-		return personalValue;
-	}
-
-	public double getListPrice() {
-		return listPrice;
-	}
-
-	public double getCostPrice() {
-		return costPrice;
-	}
-
-	public double getRealCostPrice() {
-		return realCostPrice;
-	}
-
-	public String getAmountUnit() {
-		return amountUnit;
-	}
-
-	public String getProductDescribe() {
-		return productDescribe;
-	}
-
-	public String getResponsiblePerson() {
-		return responsiblePerson;
-	}
-
-	public String getRegister() {
-		return register;
-	}
-
-	public Date getRegisterTime() {
-		return registerTime;
-	}
-
-	public String getChecker() {
-		return checker;
-	}
-
-	public Date getCheckTime() {
-		return checkTime;
-	}
-
-	public String getCheckTag() {
-		return checkTag;
-	}
-
-	public String getChanger() {
-		return changer;
-	}
-
-	public Date getChangeTime() {
-		return changeTime;
-	}
-
-	public String getChangeTag() {
-		return changeTag;
-	}
-
-	public String getPriceChangeTag() {
-		return priceChangeTag;
-	}
-
-	public Integer getFileChangeAmount() {
-		return fileChangeAmount;
-	}
-
-	public String getDeleteTag() {
-		return deleteTag;
-	}
-
-	public String getDesignModuleTag() {
-		return designModuleTag;
-	}
-
-	public String getDesignProcedureTag() {
-		return designProcedureTag;
-	}
-
-	public String getDesignCellTag() {
-		return designCellTag;
-	}
-
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public String getProductId() {
+		return productId;
 	}
 
 	public void setProductId(String productId) {
 		this.productId = productId;
 	}
 
+	public String getProductName() {
+		return productName;
+	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
+	}
+
+	public String getFactoryName() {
+		return factoryName;
 	}
 
 	public void setFactoryName(String factoryName) {
 		this.factoryName = factoryName;
 	}
 
+	public String getFirstKindId() {
+		return firstKindId;
+	}
+
 	public void setFirstKindId(String firstKindId) {
 		this.firstKindId = firstKindId;
+	}
+
+	public String getFirstKindName() {
+		return firstKindName;
 	}
 
 	public void setFirstKindName(String firstKindName) {
 		this.firstKindName = firstKindName;
 	}
 
+	public String getSecondKindId() {
+		return secondKindId;
+	}
+
 	public void setSecondKindId(String secondKindId) {
 		this.secondKindId = secondKindId;
+	}
+
+	public String getSecondKindName() {
+		return secondKindName;
 	}
 
 	public void setSecondKindName(String secondKindName) {
 		this.secondKindName = secondKindName;
 	}
 
+	public String getThirdKindId() {
+		return thirdKindId;
+	}
+
 	public void setThirdKindId(String thirdKindId) {
 		this.thirdKindId = thirdKindId;
+	}
+
+	public String getThirdKindName() {
+		return thirdKindName;
 	}
 
 	public void setThirdKindName(String thirdKindName) {
 		this.thirdKindName = thirdKindName;
 	}
 
+	public String getProductNick() {
+		return productNick;
+	}
+
 	public void setProductNick(String productNick) {
 		this.productNick = productNick;
+	}
+
+	public String getType() {
+		return type;
 	}
 
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	public String getProductClass() {
+		return productClass;
+	}
+
 	public void setProductClass(String productClass) {
 		this.productClass = productClass;
+	}
+
+	public String getPersonalUnit() {
+		return personalUnit;
 	}
 
 	public void setPersonalUnit(String personalUnit) {
 		this.personalUnit = personalUnit;
 	}
 
+	public String getPersonalValue() {
+		return personalValue;
+	}
+
 	public void setPersonalValue(String personalValue) {
 		this.personalValue = personalValue;
 	}
 
-	public void setListPrice(double listPrice) {
+	public Double getListPrice() {
+		return listPrice;
+	}
+
+	public void setListPrice(Double listPrice) {
 		this.listPrice = listPrice;
 	}
 
-	public void setCostPrice(double costPrice) {
+	public Double getCostPrice() {
+		return costPrice;
+	}
+
+	public void setCostPrice(Double costPrice) {
 		this.costPrice = costPrice;
 	}
 
-	public void setRealCostPrice(double realCostPrice) {
+	public Double getRealCostPrice() {
+		return realCostPrice;
+	}
+
+	public void setRealCostPrice(Double realCostPrice) {
 		this.realCostPrice = realCostPrice;
+	}
+
+	public String getAmountUnit() {
+		return amountUnit;
 	}
 
 	public void setAmountUnit(String amountUnit) {
 		this.amountUnit = amountUnit;
 	}
 
+	public String getProductDescribe() {
+		return productDescribe;
+	}
+
 	public void setProductDescribe(String productDescribe) {
 		this.productDescribe = productDescribe;
+	}
+
+	public String getResponsiblePerson() {
+		return responsiblePerson;
 	}
 
 	public void setResponsiblePerson(String responsiblePerson) {
 		this.responsiblePerson = responsiblePerson;
 	}
 
+	public String getRegister() {
+		return register;
+	}
+
 	public void setRegister(String register) {
 		this.register = register;
+	}
+
+	public Date getRegisterTime() {
+		return registerTime;
 	}
 
 	public void setRegisterTime(Date registerTime) {
 		this.registerTime = registerTime;
 	}
 
+	public String getChecker() {
+		return checker;
+	}
+
 	public void setChecker(String checker) {
 		this.checker = checker;
+	}
+
+	public Date getCheckTime() {
+		return checkTime;
 	}
 
 	public void setCheckTime(Date checkTime) {
 		this.checkTime = checkTime;
 	}
 
+	public String getCheckTag() {
+		return checkTag;
+	}
+
 	public void setCheckTag(String checkTag) {
 		this.checkTag = checkTag;
+	}
+
+	public String getChanger() {
+		return changer;
 	}
 
 	public void setChanger(String changer) {
 		this.changer = changer;
 	}
 
+	public Date getChangeTime() {
+		return changeTime;
+	}
+
 	public void setChangeTime(Date changeTime) {
 		this.changeTime = changeTime;
+	}
+
+	public String getChangeTag() {
+		return changeTag;
 	}
 
 	public void setChangeTag(String changeTag) {
 		this.changeTag = changeTag;
 	}
 
+	public String getPriceChangeTag() {
+		return priceChangeTag;
+	}
+
 	public void setPriceChangeTag(String priceChangeTag) {
 		this.priceChangeTag = priceChangeTag;
+	}
+
+	public Integer getFileChangeAmount() {
+		return fileChangeAmount;
 	}
 
 	public void setFileChangeAmount(Integer fileChangeAmount) {
 		this.fileChangeAmount = fileChangeAmount;
 	}
 
+	public String getDeleteTag() {
+		return deleteTag;
+	}
+
 	public void setDeleteTag(String deleteTag) {
 		this.deleteTag = deleteTag;
+	}
+
+	public String getDesignModuleTag() {
+		return designModuleTag;
 	}
 
 	public void setDesignModuleTag(String designModuleTag) {
 		this.designModuleTag = designModuleTag;
 	}
 
+	public String getDesignProcedureTag() {
+		return designProcedureTag;
+	}
+
 	public void setDesignProcedureTag(String designProcedureTag) {
 		this.designProcedureTag = designProcedureTag;
+	}
+
+	public String getDesignCellTag() {
+		return designCellTag;
 	}
 
 	public void setDesignCellTag(String designCellTag) {
 		this.designCellTag = designCellTag;
 	}
 
+	
 }
